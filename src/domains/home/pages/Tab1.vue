eeceaf1bd44796f0c06944c5116351878011e736
joelgrod <joelgrod@gmail.com>
2021-10-01 15:40:25 +0100

    ion lists and icons

--- src/domains/home/pages/Tab1.vue
+++ src/domains/home/pages/Tab1.vue
@@ -6,7 +6,6 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Games</ion-title>
@@ -14,11 +13,29 @@
       </ion-header>
 
       <ion-list>
-        <ion-item v-for="game in gamesList" :key="game.title">
+
+        <ion-list-header class="list-header">
+          IonItem
+        </ion-list-header>
+        <ion-item v-for="game in gamesList" :key="game.id">
           <ion-label> {{ game.title }} </ion-label>
         </ion-item>
-      </ion-list>
 
+        <ion-list-header class="list-header">
+          IonItemSliding
+        </ion-list-header>
+        <ion-item-sliding v-for="game in gamesList" :key="game.id">
+          <ion-item>
+            <ion-label>{{ game.title }}</ion-label>
+          </ion-item>
+          <ion-item-options side="end">
+            <ion-item-option @click="unread(game.title)" >
+              <ion-icon :icon="heartHalfOutline"></ion-icon>
+            </ion-item-option>
+          </ion-item-options>
+        </ion-item-sliding>
+
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
@@ -33,7 +50,13 @@ import {
   IonList,
   IonItem,
   IonLabel,
+  IonItemSliding,
+  IonItemOption,
+  IonItemOptions,
+  IonListHeader,
+  IonIcon
 } from "@ionic/vue";
+import { heartHalfOutline } from "ionicons/icons"
 import { ref } from "vue";
 
 export default {
@@ -47,33 +70,59 @@ export default {
     IonList,
     IonItem,
     IonLabel,
+    IonItemSliding,
+    IonItemOption,
+    IonItemOptions,
+    IonListHeader,
+    IonIcon
   },
 
   setup() {
     const gamesList = ref([
       {
+        id: 0,
         title: "Dead Cells",
       },
       {
+        id: 1,
         title: "Disco Elysium",
       },
       {
+        id: 2,
         title: "Pathologic",
       },
       {
+        id: 3,
         title: "Oxigen not Included",
       },
       {
+        id: 4,
         title: "Encased",
       },
       {
+        id: 5,
         title: "Hades",
       },
     ]);
 
+    const unread = ( item: any ) => {
+      console.log(item);
+    };
+
     return {
+      // Icons
+      heartHalfOutline,
+      // Public properties
       gamesList,
+      // Public methods
+      unread,
     };
   },
 };
 </script>
+
+<style scoped>
+.list-header {
+  padding: 0.2rem 1rem 1rem 1rem;
+}
+</style>
