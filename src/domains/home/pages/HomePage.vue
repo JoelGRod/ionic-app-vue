8db712ec76f236ddaf7e9141de6dc60fe5558bc7
joelgrod <joelgrod@gmail.com>
2021-10-08 14:02:01 +0100

    Grid examples

--- src/domains/home/pages/HomePage.vue
+++ src/domains/home/pages/HomePage.vue
@@ -1,15 +1,13 @@
 <template>
   <ion-page>
     <ion-content>
-      <h1>Grid Tests</h1>
-      
-      <h3>Same Width - no fixed</h3>
-      
       <ion-grid>
-        <ion-row
-          class="ion-justify-content-center">
-          <ion-col class="red flex">
-            <p>Test A</p>
+        <h1>Grid Tests</h1>
+        <h2 class="ion-hide-sm-down">Hidden Text below sm breakpoint</h2>
+        <h3>Same Width - no fixed</h3>
+        <ion-row class="ion-justify-content-center">
+          <ion-col class="red ion-text-justify ion-text-capitalize">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
           </ion-col>
           <ion-col class="blue flex">
             <p>Test B</p>
@@ -23,11 +21,10 @@
         </ion-row>
       </ion-grid>
 
-      <h3>Same Width - fixed</h3>
-
       <ion-grid :fixed="true">
-        <ion-row
-          class="ion-justify-content-center">
+        <h3>Same Width - fixed</h3>
+
+        <ion-row class="ion-justify-content-center">
           <ion-col class="red flex">
             <p>Test A</p>
           </ion-col>
@@ -41,13 +38,10 @@
             <p>Test D</p>
           </ion-col>
         </ion-row>
-      </ion-grid>
 
-      <h3>Different Width - fixed</h3>
+        <h3>Different Width - fixed</h3>
 
-      <ion-grid :fixed="true">
-        <ion-row
-          class="ion-justify-content-center">
+        <ion-row class="ion-justify-content-center">
           <ion-col class="red flex" size="3">
             <p>Test A</p>
           </ion-col>
@@ -61,13 +55,10 @@
             <p>Test D</p>
           </ion-col>
         </ion-row>
-      </ion-grid>
 
-      <h3>Auto Width - fixed</h3>
+        <h3>Auto Width - fixed</h3>
 
-      <ion-grid :fixed="true">
-        <ion-row
-          class="ion-justify-content-center">
+        <ion-row class="ion-justify-content-center">
           <ion-col class="red flex">
             <p>Test A</p>
           </ion-col>
@@ -81,68 +72,144 @@
             <p>Test D</p>
           </ion-col>
         </ion-row>
-      </ion-grid>
 
-      <h3>Responsive width - fixed</h3>
+        <h3>Responsive width - fixed</h3>
 
-      <ion-grid :fixed="true">
-        <ion-row
-          class="ion-justify-content-center">
+        <ion-row class="ion-justify-content-center">
           <ion-col class="red flex" size="12" size-sm="6" size-md="3" size-lg>
             <p>Test A</p>
           </ion-col>
           <ion-col class="blue flex" size="12" size-sm="6" size-md="2" size-lg>
             <p>Test B</p>
           </ion-col>
-          <ion-col class="purple flex" size="12" size-sm="6" size-md="2" size-lg>
+          <ion-col
+            class="purple flex"
+            size="12"
+            size-sm="6"
+            size-md="2"
+            size-lg>
             <p>Test C</p>
           </ion-col>
           <ion-col class="green flex" size="12" size-sm="6" size-md="3" size-lg>
             <p>Test D</p>
           </ion-col>
         </ion-row>
-      </ion-grid>
 
-      <h3>Responsive width + offset - fixed</h3>
+        <h3>Responsive width + offset - fixed</h3>
 
-      <ion-grid :fixed="true">
-        <ion-row
-          class="ion-justify-content-center">
+        <ion-row class="ion-justify-content-center">
           <ion-col class="red flex" size="12" size-sm="6" size-md="3" size-lg>
             <p>Test A</p>
           </ion-col>
-          <ion-col class="blue flex" size="12" size-sm="6" size-md="2" size-lg offset-md="0.2">
+          <ion-col
+            class="blue flex"
+            size="12"
+            size-sm="6"
+            size-md="2"
+            size-lg
+            offset-md="0.2">
             <p>Test B</p>
           </ion-col>
-          <ion-col class="purple flex" size="12" size-sm="6" size-md="2" size-lg offset-md="0.2">
+          <ion-col
+            class="purple flex"
+            size="12"
+            size-sm="6"
+            size-md="2"
+            size-lg
+            offset-md="0.2">
             <p>Test C</p>
           </ion-col>
-          <ion-col class="green flex" size="12" size-sm="6" size-md="3" size-lg offset-md="0.2">
+          <ion-col
+            class="green flex"
+            size="12"
+            size-sm="6"
+            size-md="3"
+            size-lg
+            offset-md="0.2">
             <p>Test D</p>
           </ion-col>
         </ion-row>
-      </ion-grid>
 
-      <h3>Responsive width + Push and Pull - fixed</h3>
+        <h3>Responsive width + Push and Pull - fixed</h3>
 
-      <ion-grid :fixed="true">
-        <ion-row
-          class="ion-justify-content-center">
-          <ion-col class="red flex" size="12" size-sm="6" size-md="3" size-lg push-md="2.2">
+        <ion-row class="ion-justify-content-center">
+          <ion-col
+            class="red flex"
+            size="12"
+            size-sm="6"
+            size-md="3"
+            size-lg
+            push-md="2.2">
             <p>Test A</p>
           </ion-col>
-          <ion-col class="blue flex" size="12" size-sm="6" size-md="2" size-lg offset-md="0.2" pull-md="3.2">
+          <ion-col
+            class="blue flex"
+            size="12"
+            size-sm="6"
+            size-md="2"
+            size-lg
+            offset-md="0.2"
+            pull-md="3.2">
             <p>Test B</p>
           </ion-col>
-          <ion-col class="purple flex" size="12" size-sm="6" size-md="2" size-lg offset-md="0.2">
+          <ion-col
+            class="purple flex"
+            size="12"
+            size-sm="6"
+            size-md="2"
+            size-lg
+            offset-md="0.2">
             <p>Test C</p>
           </ion-col>
-          <ion-col class="green flex" size="12" size-sm="6" size-md="3" size-lg offset-md="0.2">
+          <ion-col
+            class="green flex"
+            size="12"
+            size-sm="6"
+            size-md="3"
+            size-lg
+            offset-md="0.2">
             <p>Test D</p>
           </ion-col>
         </ion-row>
-      </ion-grid>
 
+        <h3>Align Items - fixed</h3>
+
+        <ion-row
+          class="ion-justify-content-center ion-align-items-center custom-height-row white">
+          <ion-col
+            class="red flex custom-height-col ion-align-self-start"
+            size="12"
+            size-md="3"
+            size-lg>
+            <p>Test A</p>
+          </ion-col>
+          <ion-col
+            class="blue flex custom-height-col"
+            size="12"
+            size-md="2"
+            size-lg
+            offset-md="0.2">
+            <p>Test B</p>
+          </ion-col>
+          <ion-col
+            class="purple flex custom-height-col ion-align-self-end"
+            size="12"
+            size-md="2"
+            size-lg
+            offset-md="0.2">
+            <p>Test C</p>
+          </ion-col>
+          <ion-col
+            class="green flex custom-height-col"
+            size="12"
+            size-md="3"
+            size-lg
+            offset-md="0.2">
+            <p>Test D</p>
+          </ion-col>
+        </ion-row>
+
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
@@ -171,24 +238,33 @@ export default {
 </script>
 
 <style scoped>
-  .text-color {
-    color: #333333;
-  }
-  .blue {
-    background-color: blue;
-  }
-  .red {
-    background-color: red;
-  }
-  .green {
-    background-color: green;
-  }
-  .purple {
-    background-color: purple;
-  }
-  .flex {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.text-color {
+  color: #333333;
+}
+.blue {
+  background-color: blue;
+}
+.red {
+  background-color: red;
+}
+.green {
+  background-color: green;
+}
+.purple {
+  background-color: purple;
+}
+.white {
+  background-color: #f3f3f3;
+}
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.custom-height-row {
+  height: 10rem;
+}
+.custom-height-col {
+  height: 4rem;
+}
 </style>
